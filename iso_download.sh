b9295b4d853ec0e619660e5e0b39db495641cd53
kse201 <kse.201@gmail.com>
2019-02-04 13:11:45 +0900

    feature: add retry in iso downloading

--- iso_download.sh
+++ iso_download.sh
@@ -6,4 +6,7 @@ md5sum=$(echo $md5sums | awk '/iso/ {print $1}')
 iso=$(echo $md5sums | awk '/iso/ {print $2}')
 
 wget -O $iso \
+  --tries=0  \
+  --retry-connrefused \
+  --continue \
   "http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/iso/latest/$iso"
